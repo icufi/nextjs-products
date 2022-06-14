00d208d2aabf64235890d3107e4f733206236303
Max Girteit <icufishmg@gmail.com>
2022-06-14 14:03:14 -0600

    added getstaticpaths and getstaticprops

---
+++ pages/[productId].js
@@ -0,0 +1,56 @@
+import { Fragment } from 'react';
+import path from 'path';
+import fs from 'fs/promises';
+
+const ProductDetailPage = (props) => {
+  const { loadedProduct } = props;
+
+  return (
+    <Fragment>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </Fragment>
+  );
+};
+
+export async function getData() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productId = params.productId;
+
+  const data = await getData();
+
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+
+
+export async function getStaticPaths() {
+
+      const data = await getData();
+
+      const ids = data.products.map(product => product.id)
+
+      const pathsWithParams = ids.map(id => ({params: {productId: id}}))
+
+  return {
+    paths: pathsWithParams,
+    fallback: 'blocking',
+  };
+}
+
+export default ProductDetailPage;
