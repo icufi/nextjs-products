0cb400c4fbf6b1b744746702ee7517a00c882f66
Max Girteit <icufishmg@gmail.com>
2022-06-17 20:46:45 -0600

    return object 'not found' added

--- pages/[productId].js
+++ pages/[productId].js
@@ -30,6 +30,10 @@ export async function getStaticProps(context) {
 
   const product = data.products.find((product) => product.id === productId);
 
+  if (!product) {
+    return {notFound: true}
+  }
+
   return {
     props: {
       loadedProduct: product,
